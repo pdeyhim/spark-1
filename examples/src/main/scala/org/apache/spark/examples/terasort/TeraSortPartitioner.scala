c449b4be00d50ed6197069dbc9b83e98c9efaf61
pdeyhim <deyhim@gmail.com>
2014-07-03 14:12:00 -0700

    removed Kinesis added terasort

--- examples/src/main/scala/org/apache/spark/examples/terasort/TeraSortPartitioner.scala
+++ examples/src/main/scala/org/apache/spark/examples/terasort/TeraSortPartitioner.scala
@@ -15,16 +15,31 @@
  * limitations under the License.
  */
 
-package org.apache.spark.streaming.kinesis
+package org.apache.spark.examples.terasort
 
-import org.apache.spark.streaming.{StreamingContext, TestSuiteBase}
-import org.apache.spark.storage.StorageLevel
+import com.google.common.primitives.Longs
 
-class KinesisStreamSuite extends TestSuiteBase {
+import org.apache.spark.Partitioner
 
-  test("Kinesis input stream") {
-    val ssc = new StreamingContext(master, framework, batchDuration)
-    val test1 = KinesisUtils.createStream(ssc, accesskey="x",accessSecretKey="y",kinesisStream="z")
-    ssc.stop()
-  } 
+/**
+ * Partitioner for terasort. It uses the first seven bytes of the byte array to partition
+ * the key space evenly.
+ */
+case class TeraSortPartitioner(numPartitions: Int) extends Partitioner {
+
+  import TeraSortPartitioner._
+
+  val rangePerPart = (max - min) / numPartitions
+
+  override def getPartition(key: Any): Int = {
+    val b = key.asInstanceOf[Array[Byte]]
+    val prefix = Longs.fromBytes(0, b(0), b(1), b(2), b(3), b(4), b(5), b(6))
+    (prefix / rangePerPart).toInt
+  }
+}
+
+
+object TeraSortPartitioner {
+  val min = Longs.fromBytes(0, 0, 0, 0, 0, 0, 0, 0)
+  val max = Longs.fromBytes(0, -1, -1, -1, -1, -1, -1, -1)  // 0xff = -1
 }
